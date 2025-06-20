6ed4529d8003930bb760c381c1f96225a739fd7c
Suvo Ganguli <subhabrata.ganguli@gmail.com>
2025-06-20 18:11:35 -0400

    updated 05 and inference to remove headers from X_val

--- inference.py
+++ inference.py
@@ -1,20 +1,25 @@
-import joblib
-import os
 import pandas as pd
+import pickle
+import os
+from io import StringIO
 
 def model_fn(model_dir):
     model_path = os.path.join(model_dir, "model.pkl")
-    return joblib.load(model_path)
+    with open(model_path, "rb") as f:
+        model = pickle.load(f)
+    return model
 
-def input_fn(request_body, content_type):
-    if content_type == "text/csv":
-        return pd.read_csv(pd.compat.StringIO(request_body))
-    raise ValueError("Unsupported content type: {}".format(content_type))
+def input_fn(request_body, request_content_type):
+    if request_content_type == "text/csv":
+        return pd.read_csv(StringIO(request_body), header=None)
+    else:
+        raise ValueError("Unsupported content type: {}".format(request_content_type))
 
 def predict_fn(input_data, model):
     return model.predict(input_data)
 
-def output_fn(prediction, accept):
-    if accept == "text/csv":
-        return ",".join(str(x) for x in prediction), "text/csv"
-    raise ValueError("Unsupported accept type: {}".format(accept))
+def output_fn(prediction, content_type):
+    if content_type == "text/csv":
+        return "\n".join(str(x) for x in prediction)
+    else:
+        raise ValueError("Unsupported content type: {}".format(content_type))
